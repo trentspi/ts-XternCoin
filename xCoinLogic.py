cf73d66bce93b2224f385bb0041e36c0b839999d
Trent Spice <tspice23@att.net>
2016-11-01 08:23:19 -0400

    Add files via upload

---
+++ xCoinLogic.py
@@ -0,0 +1,45 @@
+'''
+Trenton Spice
+'''
+
+from random import randint
+
+class xCoin(object):
+    def __init__(self):
+        self.userID = '200169'
+        self.userKeys = 0
+        self.xRand = randint(0,10)
+
+
+    def randNum(self):
+        return randint(0,10)
+
+    def HandleGuess(self, uid, guess):
+        uid = str(uid)
+        guess = int(guess)
+        if uid == self.userID:
+            if guess == self.xRand:
+                self.userKeys += 1
+                self.xRand = self.randNum()
+                print("Correct Guess, +1 XternCoin!")
+            else:
+                print("Incorrect Guess")
+
+    def GetCoins(self, userID):
+        userID = str(userID)
+        if userID == self.userID:
+            return self.userKeys
+
+    def StartGuessing(self):
+        for i in range(100):
+            randGuess = randint(0,10)
+            print("\nGuessing...")
+            self.HandleGuess(self.userID, randGuess)
+            print("XternCoins : " + str(self.GetCoins(self.userID)))
+
+def main():
+    c = xCoin()
+    c.StartGuessing()
+
+if __name__ == "__main__":
+    main()
